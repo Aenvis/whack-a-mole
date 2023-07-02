f9805fade5049c2a0f205ded9816f7f9b5270daa
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-07-02 11:56:21 +0200

    Add working mole selection under mouse position using raycasting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,16 +17,19 @@ public class PlayerController : MonoBehaviour
         
         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+
+        if (!Physics.Raycast(ray, out hit, Mathf.Infinity, targetLayer)) return;
         
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity,
-                targetLayer))
+        if (hit.collider.CompareTag("Mole"))
         {
-            Debug.DrawRay(mousePosition, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, (hit.transform.position - transform.position) * hit.distance,
+                Color.yellow);
             Debug.Log("Did Hit");
         }
         else
         {
-            Debug.DrawRay(mousePosition, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.DrawRay(transform.position, (hit.transform.position - transform.position) * hit.distance,
+                Color.white);
             Debug.Log("Did not Hit");
         }
     }
